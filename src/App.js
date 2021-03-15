70c2e4646f2964e35eaead24fbeefe7aee89c392
Jon Flores <61673020+jonflores1093@users.noreply.github.com>
2021-03-15 17:56:57 -0500

    (Fake Member) Removed unnecessary emojis.

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import IncrementButton from "./demos/states/custom-increment";
 function App() {
   return (
     <main>
-      <h1>Welcome! 👋👋</h1>
+      <h1>Welcome!</h1>
 
       <h2>Custom Increment</h2>
 
